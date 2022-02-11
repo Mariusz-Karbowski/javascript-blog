9afada898264c1a7db286ece4f6b936191498f25
Mariusz-Karbowski <m.karbowski@outlook.com>
2022-02-11 15:38:35 +0100

    Added article display after link click

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function titleClickHandler(event) {
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
 
@@ -14,24 +15,31 @@ function titleClickHandler(event) {
 
     /* [DONE] add class 'active' to the clicked link */
 
-    console.log('clickedElement', clickedElement);
-    
+    console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
     
 
     /* [DONE] remove class 'active' from all articles */
-
+    
     const activeArticles = document.querySelectorAll('.posts .active');
 
     for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
 }    
 
-/* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+    
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log('articleSelector:', articleSelector);
 
-/* find correct article using the selector (value of 'href' attribute) */
+    /* [DONE] find correct article using the selector (value of 'href' attribute) */
+    
+    const targetArticle = document.querySelector(articleSelector);
+    console.log('targetArticle:', targetArticle);
 
-/* add class 'active' to the correct article */
+    /* add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
