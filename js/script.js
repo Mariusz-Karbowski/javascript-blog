131c5f89a36834b4eca53c404d5cc7d3fea64b7c
Mariusz-Karbowski <m.karbowski@outlook.com>
2022-02-11 13:01:42 +0100

    Added class removal

--- js/script.js
+++ js/script.js
@@ -2,19 +2,31 @@
 
 function titleClickHandler(event) {
     console.log('Link was clicked!', event);
-}
 
-/* remove class 'active' fromm all article links */
+    /* remove class 'active' fromm all article links */
+
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
 
 /* add class 'active' to the clicked link */
 
-/* remove class 'active' from all articles */
+    /* remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.posts .active');
+
+    for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+}    
 
 /* get 'href' attribute from the clicked link */
 
 /* find correct article using the selector (value of 'href' attribute) */
 
 /* add class 'active' to the correct article */
+}
 
 const links = document.querySelectorAll('.titles a');
 
